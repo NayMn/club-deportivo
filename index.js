a8a445dd15a00aefa3647451b5740f0ccee9b786
najira <najira_daniela@hotmail.com>
2024-04-24 16:39:10 -0400

    creacion ruta deporte

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express();
 const __dirname = import.meta.dirname;
 app.use(express.static(__dirname + '/public'));
 
+app.get("/deporte", (req, res) => {
+    res.send("deportes guardados")
+})
+
+
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
